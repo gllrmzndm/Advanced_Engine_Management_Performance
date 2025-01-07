b56312ef459930a656b59a35d4f871158981d0d5
gllrmzndm <guillermozaandam@hotmail.com>
2025-01-07 23:24:05 +0100

    POP Ansible

--- Linux_Pop_OS/pop_install.sh
+++ Linux_Pop_OS/pop_install.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+################################################################################
+#                              scriptTemplate                                  #
+#                                                                              #
+# Installing software for Ubuntu/PopOS!                                        #
+#                                                                              #
+#                                                                              #
+# Usage: sudo -u $USER ./MYINSTALLATION.sh                                     #
+#                           Original code. This is a template for creating     #
+#                           new Bash shell scripts.                            #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+################################################################################
+
+#Updating and upgrading Ubuntu
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-sudo apt-get install -y ripgrep
-sudo apt-get install -y ranger
+#Normal apps
 sudo apt-get install -y gnupg2
 sudo apt-get install -y pass
 sudo apt-get install -y shellcheck
@@ -24,18 +38,24 @@ sudo apt-get install -y scribus
 sudo apt-get install -y krita
 sudo apt-get install -y darktable
 sudo apt-get install -y graphviz
-
+# sudo apt-get install -y obs-studio
+#sudo apt-get install -y docker
+#sudo apt-get install -y docker-compose
+#sudo apt-get install -y zram
+#sudo apt-get install -y clamav clamtk
 sudo apt-get install -y google-chrome-stable
 sudo apt-get install -y chrome-gnome-shell
 sudo apt-get install -y peek
 sudo apt-get install -y gnome-shell-extension-weather gnome-shell-extension-gsconnect-browsers
 sudo apt-get install -y lollypop
 sudo apt-get install -y shotwell
-
+#sudo apt-get install -y gnome-firmware
+sudo apt-get install -y clamav clamtk clamdscan
 sudo apt-get install -y remmina
 sudo apt-get install -y transmission
 sudo apt-get install -y deja-dup
 sudo apt-get install -y ansible
+#sudo apt-get install -y tlp tlp-rdw
 sudo apt-get install -y dislocker
 sudo apt-get install -y neovim
 sudo apt-get install -y gnome-books
@@ -47,14 +67,15 @@ sudo apt-get install -y aria2
 sudo apt-get install -y simplescreenrecorder
 sudo apt-get install -y openshot
 sudo apt-get install -y install gnome-documents
-sudo apt-get install -y neovim
 
-yes 3 | sudo update-alternatives --config editor
+yes 4 | sudo update-alternatives --config editor
 
 flatpak install flathub de.haeckerfelix.Fragments -y
 
 sudo apt-get install -y flameshot
 flameshot config -f %T_ %e-%m-%Y
+
+#sudo apt-get install -y ubuntu-restricted-extras
 
 sudo apt-get install -y virt-manager libguestfs-tools gnome-boxes
 sudo apt-get install -y qemu qemu-kvm bridge-utils
@@ -62,10 +83,6 @@ sudo adduser $USER libvirt
 sudo adduser $USER libvirt-qemu
 
 # Automatic updates https://www.cyberciti.biz/faq/how-to-set-up-automatic-updates-for-ubuntu-linux-18-04/
-
-# optional neovim for kickstart
-# sudo add-apt-repository ppa:neovim-ppa/unstable
-# sudo apt update
 
 #sudo apt-get install unattended-upgrades apt-listchanges
 #sudo dpkg-reconfigure -plow unattended-upgrades
@@ -98,6 +115,7 @@ sudo adduser $USER libvirt-qemu
 #vagrant plugin install vagrant-hostsup
 #sudo apt-get install -y gnome-shell-extension-weather gnome-shell-extension-gsconnect-browsers
 
+
 # https://www.pling.com/p/1175480/
 # https://www.gnome-look.org/p/1148692/
 # https://extensions.gnome.org/extension/1319/gsconnect/
@@ -106,4 +124,3 @@ sudo adduser $USER libvirt-qemu
 
 # https://askubuntu.com/questions/968103/disable-the-default-app-key-supernum-functionality-on-ubuntu-17-10-and-later
 # https://askubuntu.com/questions/617589/ubuntu-15-04-jumps-into-standby-after-login
-# https://itsfoss.com/curtail-image-compress/ Image compressions
